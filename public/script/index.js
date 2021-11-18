0c98a65a0396ddb99338019fa30271fc34a56935
Manuel <mayerga4@gmail.com>
2021-11-18 23:39:28 +0100

    comenzando express

---
+++ public/script/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen()
+
+//SEGUIR CON EL CURSO: https://www.youtube.com/watch?v=L72fhGm1tfE
