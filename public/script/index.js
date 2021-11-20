993dd23c3fd941d93ecc3aad9335ea38674eb694
Manuel <mayerga4@gmail.com>
2021-11-20 12:23:33 +0100

    creacion carpeta estática

--- public/script/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen()
-
-//SEGUIR CON EL CURSO: https://www.youtube.com/watch?v=L72fhGm1tfE
